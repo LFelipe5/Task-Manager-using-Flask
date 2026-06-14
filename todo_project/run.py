5c36458381f0bfd4d6cffae219634a0bba2fc7d9
luiz.oliveira <luizfelipedpt@gmail.com>
2026-06-14 20:31:38 -0300

    feat: compose, ci, require, prometheus, grafana

--- todo_project/run.py
+++ todo_project/run.py
@@ -1,4 +1,7 @@
 from todo_project import app
+from prometheus_flask_exporter import PrometheusMetrics
+
+metrics = PrometheusMetrics(app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
